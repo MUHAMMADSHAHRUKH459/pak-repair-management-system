d2f1dfc7b9f20cf1dcc67ba98928ffcaf65f4f00
MUHAMMADSHAHRUKH459 <freelacing123@gmail.com>
2026-04-22 04:11:06 +0500

    pusssssssshhhhhhhhhhhhhhh

--- prisma/prisma.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'prisma/config'
-
-export default defineConfig({
-  datasource: {
-    url: "postgresql://postgres.fcljgrbhmlpoivfwlxzm:Anikkhan1234!@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres",
-  },
-})
